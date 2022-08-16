e8f1bc215870246cb58f2e656fea8c2d340f54f4
ElenaG <freshelf@yandex.ru>
2022-08-17 01:10:52 +0500

    add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,20 @@
+﻿// вход - число, выход !числа.
+
+int Factorial(int n)
+{
+    int result = 1;
+    for(int number = 1; number <= n; number++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+void PrintFactorial(int n)
+{
+    Console.WriteLine($"Факториал числа {n} равен {Factorial(n)}");
+}
+
+PrintFactorial(10);
+PrintFactorial(5);
+PrintFactorial(0);
