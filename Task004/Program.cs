8dfce105f12838877e5cb883a7406f0153f5def6
ElenaG <freshelf@yandex.ru>
2022-08-17 01:11:22 +0500

    add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,23 @@
+﻿// вывести массив из 8 элементов, заполненный 0 и 1 в случайном порядке.
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+} 
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int index = 0; index < count; index++)
+    {
+        Console.Write(array[index] + " " );
+    }
+}
+
+FillArray(8);
+PrintArray(FillArray(8));
