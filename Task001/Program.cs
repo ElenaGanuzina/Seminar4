685d169b7e8e022c65ce752ba71f3c3475778c53
ElenaG <freshelf@yandex.ru>
2022-08-17 01:09:08 +0500

    add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,20 @@
+﻿// вход - число А, выход - сумма чисел от 1 до А.
+
+int Sum(int a)
+{
+    int result = 0;
+    for(int number = 0; number <= a; number++)
+    {
+        result += number;
+    }
+    return result;
+}
+
+void PrintSum(int a)
+{
+    Console.WriteLine($"Сумма числе от 1 до {a} равна {Sum(a)}");
+}
+
+PrintSum(10);
+PrintSum(-5);
+PrintSum(0);
