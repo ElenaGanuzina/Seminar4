79b6a42cf9847e016188550e8571b252364d167e
ElenaG <freshelf@yandex.ru>
2022-08-17 01:10:13 +0500

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿//вход - число, выход - количество цифр в числе.
+
+int Figures(int number)
+{
+    int result = 0;
+    while(number != 0)
+    {
+        number = number / 10;
+        result++;
+    }
+    return result;
+}
+
+void PrintFigures(int number)
+{
+    Console.WriteLine(Figures(number));
+}
+
+PrintFigures(12345);
+PrintFigures(2516);
+PrintFigures(73);
